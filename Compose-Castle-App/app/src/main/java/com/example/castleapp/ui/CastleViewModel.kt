c8e141db285d4c844c5ed48c1d75f27c1f53a162
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-27 21:00:40 +0900

    Add suspend function.

--- Compose-Castle-App/app/src/main/java/com/example/castleapp/ui/CastleViewModel.kt
+++ Compose-Castle-App/app/src/main/java/com/example/castleapp/ui/CastleViewModel.kt
@@ -3,10 +3,12 @@ package com.example.castleapp.ui
 import androidx.lifecycle.ViewModel
 import com.example.castleapp.data.WarriorRepository
 import com.example.castleapp.model.Warrior
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class CastleViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(CastleUiState())
@@ -15,7 +17,7 @@ class CastleViewModel : ViewModel() {
     init {
         _uiState.update { currentState ->
             currentState.copy(
-                warriors = WarriorRepository.getCastleWarriors
+                warriors = WarriorRepository.getCastleWarriors,
             )
         }
     }
@@ -28,11 +30,22 @@ class CastleViewModel : ViewModel() {
         }
     }
 
+    suspend fun moveToCastleWarriors() {
+        coroutineScope {
+            launch { _uiState.value.warriors[0].moveToCastle() }
+            launch { _uiState.value.warriors[1].moveToCastle() }
+        }
+    }
+
     fun returnWarriors() {
         _uiState.update { currentState ->
             currentState.copy(
                 isCalled = false
             )
+        }
+
+        _uiState.value.warriors.forEach {
+            it.returnToHome()
         }
     }
 
