b5bc8475434ccd5caf6672dbdac27f640e180b2e
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-27 16:36:23 +0900

    Remove class keyword.

--- Compose-Castle-App/app/src/main/java/com/example/castleapp/data/WarriorRepository.kt
+++ Compose-Castle-App/app/src/main/java/com/example/castleapp/data/WarriorRepository.kt
@@ -3,7 +3,7 @@ package com.example.castleapp.data
 import com.example.castleapp.model.Cavalry
 import com.example.castleapp.model.Knight
 
-object class WarriorRepository {
+object WarriorRepository {
     val getCastleWarriors = listOf(
         Knight(),
         Cavalry()
