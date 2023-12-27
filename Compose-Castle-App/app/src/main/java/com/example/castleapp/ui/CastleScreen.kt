693a7ce7cd41220e6d89e066890650f9e402540f
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-27 16:28:26 +0900

    Create CastleScreen.

---
+++ Compose-Castle-App/app/src/main/java/com/example/castleapp/ui/CastleScreen.kt
@@ -0,0 +1,19 @@
+package com.example.castleapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.castleapp.ui.theme.CastleAppTheme
+
+@Composable
+fun CastleScreen() {
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CastleAppTheme {
+        CastleScreen()
+    }
+}
