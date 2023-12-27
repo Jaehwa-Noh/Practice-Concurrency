66c5c321e7f8dc7062ab0142744b29aa4c5417d0
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-27 20:59:06 +0900

    Create method and property at Warrior and Knight and Cavalry

--- Compose-Castle-App/app/src/main/java/com/example/castleapp/model/Warrior.kt
+++ Compose-Castle-App/app/src/main/java/com/example/castleapp/model/Warrior.kt
@@ -1,28 +1,42 @@
 package com.example.castleapp.model
 
-abstract class Warrior {
-    var location: Int = 0
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
-    abstract fun moveToCastle()
+abstract class Warrior {
+    abstract var location: Float
+
+    abstract suspend fun moveToCastle()
     abstract fun returnToHome()
 }
 
-class Knight(): Warrior() {
-    override fun moveToCastle() {
+class Knight : Warrior() {
+    override var location by mutableFloatStateOf(0f)
+    override suspend fun moveToCastle() {
 
+        while (location in 0f..99f) {
+            delay(500)
+            location += 3
+        }
     }
 
     override fun returnToHome() {
-
+        location = 0f
     }
 }
 
-class Cavalry(): Warrior() {
-    override fun moveToCastle() {
-
+class Cavalry : Warrior() {
+    override var location by mutableFloatStateOf(0f)
+    override suspend fun moveToCastle() {
+        while (location in 0f..99f) {
+            delay(300)
+            location += 5
+        }
     }
 
     override fun returnToHome() {
-
+        location = 0f
     }
 }
