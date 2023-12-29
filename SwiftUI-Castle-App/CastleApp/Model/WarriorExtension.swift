2a9fc03682058079633fd0e572a8d2decebf3af4
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-29 17:12:49 +0900

    Create Warrior extension whereAreYou property.

---
+++ SwiftUI-Castle-App/CastleApp/Model/WarriorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  WarriorExtension.swift
+//  CastleApp
+//
+//  Created by Jaehwa Noh on 12/29/23.
+//
+
+extension Warrior {
+    var whereAreYou: Float {
+        return Float(location / 100)
+    }
+}
