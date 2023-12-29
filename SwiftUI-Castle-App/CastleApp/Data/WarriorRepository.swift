005731b3b8fa2e248624a42f8735a1c179c81ed3
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-29 17:19:37 +0900

    Create WarriorRepository.

---
+++ SwiftUI-Castle-App/CastleApp/Data/WarriorRepository.swift
@@ -0,0 +1,13 @@
+//
+//  WarriorRepository.swift
+//  CastleApp
+//
+//  Created by Jaehwa Noh on 12/29/23.
+//
+
+struct WarriorRepository {
+    static let getcastleWarriors: [Warrior] = [
+        Knight(delayTime: 500),
+        Cavalry(delayTime: 300)
+    ]
+}
